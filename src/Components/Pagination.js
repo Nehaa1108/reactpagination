bcc05841d2a92658306c70a70118b403c7f9562b
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-03-23 11:39:55 -0400

    added api

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import ReactPaginate from 'react-paginate'
 import ReactPagination from 'react-paginate'
 function Pagination()
@@ -6,7 +6,20 @@ function Pagination()
 
   const  [items ,setItems] = useState([])
 
+  useEffect(() => {
+   const getUsers = async ()=>
+   {
+    const res = await fetch(
+      `http://localhost:3001/users`
+    )
+    const data = await res.json()
+    setItems(data)
+   }
+   getUsers()
+  }, [])
   
+console.log(items)
+
   const handlePageClick=(e)=>
   {
     // alert("clicked")
@@ -17,6 +30,20 @@ function Pagination()
   return (
     <div className="container">
       <h4>pagination</h4>
+
+{items.map((item)=>
+{
+  return <div className='col-sm-6 col-md-4  v  my-2'>
+    <div className='card shadow-sm w-100' style={{minHeight:225}}>
+      <div className='card-body'>
+        <h5 className='card-title text-center h2'>Id:</h5>
+        <h6 className='card-subtitle mb-2 text-muted text-center'>email</h6>
+        <p className='card-text'>comment</p>
+      </div>
+    </div>
+  </div>
+})}
+
 
       <ReactPaginate
       previousLabel ={'previous'} 
